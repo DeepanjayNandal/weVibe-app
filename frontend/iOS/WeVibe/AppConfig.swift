f1f40949b39cb0de7d54b4928e0b2730586e2a28
amurark3 <amurark3@asu.edu>
2026-03-05 08:40:52 -0700

    Add FirebaseCrashlytics integration and update Info.plist for URL schemes

--- frontend/iOS/WeVibe/AppConfig.swift
+++ frontend/iOS/WeVibe/AppConfig.swift
@@ -7,11 +7,11 @@ enum AppConfig {
     // MARK: - Firebase
     /// Firebase Hosting domain used in ActionCodeSettings for email verification deep links.
     /// Format: https://<PROJECT-ID>.firebaseapp.com
-    /// PROJECT_IDs are sourced from GoogleService-Info-Dev.plist (wewibe-dev) and
-    /// GoogleService-Info-Prod.plist (wewibe-prod).
+    /// PROJECT_IDs are sourced from GoogleService-Info-Dev.plist (wevibe-dev) and
+    /// GoogleService-Info-Prod.plist (wevibe-prod).
     #if DEBUG
-    static let firebaseHostingDomain = "https://wewibe-dev.firebaseapp.com"
+    static let firebaseHostingDomain = "https://wevibe-dev.firebaseapp.com"
     #else
-    static let firebaseHostingDomain = "https://wewibe-prod.firebaseapp.com"
+    static let firebaseHostingDomain = "https://wevibe-prod.firebaseapp.com"
     #endif
 }
